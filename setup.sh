6fd421c50a59b19fb18aeaa00c3b789d3a14a416
Steven Iannelli <steveonair@gmail.com>
2018-04-08 15:08:04 -0400

    Add status messages

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+echo -n copying .vimrc..
 mv ~/.vimrc ~/.vimrc.backup.$(date +"%s")
 echo "source ~/dotfiles/vim/.vimrc" > ~/.vimrc
+echo " [COMPLETE]"
 
+echo -n copying .tmux.conf..
 mv ~/.tmux.conf ~/.tmux.conf.$(date +"%s")
 cp -f ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+echo " [COMPLETE]"
