3a5e55d900a2962b57af0cd813c4e750a1c9293c
Steven Iannelli <siannelli@siannelli-3064.local>
2018-04-08 20:04:08 -0400

    Adding zsh configuration

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-echo -n backing up existing .vimrc..
+echo backing up existing ~/.config/nvim/init.vim..
+mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.$(date +"%s")
+echo backing up existing .vimrc..
 mv ~/.vimrc ~/.vimrc.backup.$(date +"%s")
 echo "source ~/dotfiles/vim/vimrc" > ~/.vimrc
-echo " [COMPLETE]"
+mkdir -p ~/.config/nvim
+echo "source ~/.vimrc" > ~/.config/nvim/init.vim
 
-echo -n backing up existing .tmux.conf..
+echo backing up existing .tmux.conf..
 mv ~/.tmux.conf ~/.tmux.conf.$(date +"%s")
 echo "source-file ~/dotfiles/tmux/tmux.conf" > ~/.tmux.conf
-echo " [COMPLETE]"
+
+echo backing up existing ~/.zshrc..
+mv ~/.zshrc ~/.zshrc.$(date +"%s")
+echo "source ~/dotfiles/zsh/zshrc" > ~/.zshrc
